d57bfb71d6eda7bdbc0e2d7c5426c84f4e6ffbe5
Elmira Grigoryan <106161682+Elmira01@users.noreply.github.com>
2022-08-12 20:24:54 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  for (let char of str) {
+  let charIndex = bracketsConfig.indexOf(char);
+  if (charIndex < 0) continue;
+  
+  let isClose = bracketsConfig.indexOf(char) % 2;
+  if (!isClose) {
+    stack.push(char);
+  } else if (isClose) {
+    let previousChar = stack.pop();
+    if (bracketsConfig.indexOf(previousChar) !== charIndex - 1) return false;
+  }
+}
+return !!str && !stack.length;
 }
